0dbd5ae2154402a09ec8b5111a9f5df90822de9a
anya-mb <anyamb@yandex.ru>
2023-07-13 00:21:16 -0700

    added openai request

--- lambdas/fill_insurance/app.py
+++ lambdas/fill_insurance/app.py
@@ -2,7 +2,6 @@ from http import HTTPStatus
 import os
 
 import json
-import boto3
 from botocore.exceptions import ClientError
 
 import boto3
@@ -10,6 +9,57 @@ import random
 import string
 
 from time import gmtime, strftime
+
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
+
+SECRET_NAME = "insurance_fills_secrets"
+SECRET_KEY_OPENAI_KEY = "open_ai_key"
+
+
+def get_openai_quote():
+    print("before key")
+    openai_key = get_secret()[SECRET_KEY_OPENAI_KEY]
+    print(f"after key, {openai_key[:5]}")
+
+    llm = OpenAI(openai_api_key=openai_key)
+    # chat = ChatOpenAI(temperature=0.5, openai_api_key=openai_key)
+
+    result = llm.predict("Give me a motivational quote")
+    print(result)
+
+    # result = chat.predict_messages([HumanMessage(content="Give me a motivational quote")])
+    # quote = result.content
+
+    quote = result  #"dummy value"
+
+    save_result = save_to_s3(quote)
+    print("saved to s3")
+    return save_result
+
+
+def save_to_s3(file_content):
+    s3 = boto3.client('s3')
+
+    bucket = os.environ['BUCKET_NAME']
+    print("bucket", bucket)
+
+    # Generate a random file name
+    file_name = ''.join(random.choices(string.ascii_lowercase, k=10))
+
+    # Upload the file to S3
+    s3.put_object(Bucket=bucket, Key=file_name, Body=file_content)
+
+    return {
+        'statusCode': 200,
+        'body': f'File uploaded successfully! file_name: {file_name}, content: {file_content}'
+    }
 
 
 def lambda_handler():  # event, context
@@ -36,7 +86,7 @@ def lambda_handler():  # event, context
 
 def get_secret():
 
-    secret_name = "insurance_fills_secrets"
+    # secret_name = "insurance_fills_secrets"
     region_name = "us-east-1"
 
     # Create a Secrets Manager client
@@ -48,7 +98,7 @@ def get_secret():
 
     try:
         get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
+            SecretId=SECRET_NAME
         )
     except ClientError as e:
         # For a list of exceptions thrown, see
@@ -63,10 +113,10 @@ def get_secret():
 
 def handler(event, context):
     try:
-
+        result = get_openai_quote()  #get_openai_quote()
         response = {
             "statusCode": HTTPStatus.OK.value,
-            "body": json.dumps(lambda_handler(), indent=2),
+            "body": json.dumps(result, indent=2),
             "headers": {
                 "content-type": "application/json",
             },
