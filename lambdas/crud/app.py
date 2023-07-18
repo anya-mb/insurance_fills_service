202d5a40500e95f078bc935018608a44746ac464
anya-mb <anyamb@yandex.ru>
2023-07-18 11:38:29 -0700

    made crud lambda more organized

--- lambdas/crud/app.py
+++ lambdas/crud/app.py
@@ -1,140 +1,50 @@
 import json
-
 import os
 import random
 import string
 from http import HTTPStatus
-
-# from time import gmtime, strftime
-
 import boto3
 
-# from botocore.exceptions import ClientError
-
-# from langchain.llms import OpenAI
-
-SECRET_NAME = "insurance_fills_secrets"
-SECRET_KEY_OPENAI_KEY = "open_ai_key"
-
-#
-# def get_openai_quote():
-#     print("before key")
-#     openai_key = get_secret()[SECRET_KEY_OPENAI_KEY]
-#
-#     llm = OpenAI(openai_api_key=openai_key)
-#
-#     result = llm.predict("Give me a motivational quote")
-#     print(result)
-#
-#     quote = result
-#
-#     save_result = save_to_s3(quote)
-#     print("saved to s3")
-#     return save_result
-
-#
-# def save_to_s3(file_content):
-#     s3 = boto3.client("s3")
-#
-#     bucket = os.environ["BUCKET_NAME"]
-#     print("bucket", bucket)
-#
-#     file_name = "".join(random.choices(string.ascii_lowercase, k=10))
-#
-#     s3.put_object(Bucket=bucket, Key=file_name, Body=file_content)
-#
-#     return {
-#         "statusCode": 200,
-#         "body": f"File uploaded successfully! file_name: {file_name}, content: {file_content}",
-#     }
-
-#
-# def lambda_handler():
-#     s3 = boto3.client("s3")
-#
-#     bucket = os.environ["BUCKET_NAME"]
-#     print("bucket", bucket)
-#
-#     file_name = "".join(random.choices(string.ascii_lowercase, k=10))
-#
-#     curr_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-#     file_content = f"This is a random file content created: {curr_time}"
-#
-#     s3.put_object(Bucket=bucket, Key=file_name, Body=file_content)
-#
-#     return {
-#         "statusCode": 200,
-#         "body": f"File uploaded successfully! file_name: {file_name}",
-#     }
-
-#
-# def get_secret():
-#     region_name = "us-east-1"
-#
-#     # Create a Secrets Manager client
-#     session = boto3.session.Session()
-#     client = session.client(service_name="secretsmanager", region_name=region_name)
-#
-#     try:
-#         get_secret_value_response = client.get_secret_value(SecretId=SECRET_NAME)
-#     except ClientError as e:
-#         raise e
-#
-#     # Decrypts secret using the associated KMS key.
-#     secret = get_secret_value_response["SecretString"]
-#
-#     return json.loads(secret)
+# SECRET_NAME = "insurance_fills_secrets"
+# SECRET_KEY_OPENAI_KEY = "open_ai_key"
 
 
-# def handler(event, context):
-#     print("Handler started")
-#
-#     try:
-#         result = get_openai_quote()
-#         response = {
-#             "statusCode": HTTPStatus.OK.value,
-#             "body": json.dumps(result, indent=2),
-#             "headers": {
-#                 "content-type": "application/json",
-#             },
-#         }
-#
-#     except Exception as e:
-#         response = {
-#             "statusCode": HTTPStatus.INTERNAL_SERVER_ERROR.value,
-#             "body": f"Exception={e}",
-#             "headers": {
-#                 "content-type": "text/plain",
-#             },
-#         }
-#
-#     return response
+def get_random_id() -> str:
+    return "".join(random.choices(string.ascii_lowercase, k=10))
+
+
+def save_to_dynamodb_table(table_name: str, data_to_add: dict):
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(table_name)
+
+    table.put_item(Item=data_to_add)
+    print(f"Successful put to the table: {table_name}")
+    print("Added data", data_to_add)
 
 
 def lambda_create_form(event, context):
     print("lambda_create_form Handler started")
 
-    body = json.loads(event.get("body"))
-    body["id"] = str(body["id"])
-    print(body)
-
-    random_id = "".join(random.choices(string.ascii_lowercase, k=10))
-
-    dynamodb = boto3.resource("dynamodb")
-
-    table_name = os.environ["CONVERSATION_TABLE_NAME"]
-    print("CONVERSATION_TABLE_NAME", table_name)
-
-    table = dynamodb.Table(table_name)
-
-    table.put_item(Item=body)
-    print("Successful put to the table")
-
     try:
-        result = {"body": body, "random_id": random_id}
+
+        conversation = json.loads(event.get("body"))
+        print("conversation:", conversation)
+
+        random_id = get_random_id()
+
+        data_to_add_to_db = {
+            "id": random_id,
+            "conversation": conversation,
+        }
+
+        table_name = os.environ["CONVERSATION_TABLE_NAME"]
+        print("CONVERSATION_TABLE_NAME", table_name)
+
+        save_to_dynamodb_table(table_name, data_to_add_to_db)
+
         response = {
             "statusCode": HTTPStatus.OK.value,
-            "body": json.dumps(result, indent=2),
+            "body": json.dumps(data_to_add_to_db, indent=2),
             "headers": {
                 "content-type": "application/json",
             },
